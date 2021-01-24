2f1bc4be98f8206e1202edd8b7bdcdf4541f8811
Xamster <47847269+Xamster3478@users.noreply.github.com>
2021-01-24 13:52:39 +0300

    Version 0.2.1

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+let counterSliders = 1;
+right.addEventListener("click", function(){
+       
+    });
+left.addEventListener("click", function(){
+       
+    });
+
+
+
+
+// Обробоботка информации\
